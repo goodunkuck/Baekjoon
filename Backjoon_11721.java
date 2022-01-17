2942a7d96c48389013b5a33ec50dac242e0f00d5
goodunkuck <dkfldid45@naver.com>
2022-01-17 23:47:43 +0900

    열 개씩 끊어 출력하기

---
+++ Backjoon_11721.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Backjoon_11721 {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String N = br.readLine();
+		
+		for(int i = 1; i <= (N.length() / 10) + 1; i++) {
+			if(i < ((N.length() / 10) + 1)) {
+				System.out.println(N.substring(((i-1) * 10), (i * 10)));				
+			}else {
+				System.out.println(N.substring(((i-1) * 10)));
+			}
+		}
+	}
+}
