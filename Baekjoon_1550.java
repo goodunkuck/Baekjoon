da88a5857efde0f6c901a7de88988bf117eb8759
goodunkuck <dkfldid45@naver.com>
2022-01-17 17:34:50 +0900

    16진수

---
+++ Baekjoon_1550.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Baekjoon_1550{
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String S = br.readLine();
+		int N = Integer.parseInt(S, 16);
+		
+		System.out.println(N);
+	}
+}
