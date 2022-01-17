c8acacf110a9b49fb0e0b666ec24d4060cf50641
goodunkuck <dkfldid45@naver.com>
2022-01-17 21:06:35 +0900

    별 찍기 - 21

--- Baekjoon_10996.java
+++ Baekjoon_10996.java
@@ -1,9 +1,35 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
 
 public class Baekjoon_10996 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		if(N == 1) {
+			System.out.println("*");
+		}else {
+			for(int i = 0; i < N * 2; i++) {
+				if(i % 2 == 0) {
+					for(int j = 0; j < N; j++) {
+						if(j % 2 == 0) {
+							System.out.print("*");
+						}else {
+							System.out.print(" ");
+						}
+					}					
+				}else {
+					for(int j = 0; j < N; j++) {
+						if(j % 2 == 1) {
+							System.out.print("*");
+						}else {
+							System.out.print(" ");
+						}
+					}
+				}
+				System.out.println();
+			}
+		}
 	}
-
 }
