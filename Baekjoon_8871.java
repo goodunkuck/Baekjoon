37336ccd780319375416d54f772ec6ab2fa35b98
goodunkuck <dkfldid45@naver.com>
2022-01-20 17:38:37 +0900

    Zadanie próbne 2

---
+++ Baekjoon_8871.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_8871 {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		System.out.println(((N + 1) * 2) + " " + ((N + 1) * 3));
+	}
+
+}
