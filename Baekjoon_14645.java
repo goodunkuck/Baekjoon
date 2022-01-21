c391d95be3a58a35b2a33256ed18bc42cb6a5776
goodunkuck <dkfldid45@naver.com>
2022-01-22 01:08:48 +0900

    와이버스 부릉부릉

---
+++ Baekjoon_14645.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_14645 {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st1 = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st1.nextToken());
+			int B = Integer.parseInt(st1.nextToken());
+		}
+		
+		System.out.println("ºñ¿ÍÀÌ");
+	}
+
+}
