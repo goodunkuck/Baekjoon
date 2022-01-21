d764c095baf5f237d8f8f2e5e7c907c3537da275
goodunkuck <dkfldid45@naver.com>
2022-01-22 01:10:52 +0900

    조별과제를 하려는데 조장이 사라졌다

---
+++ Baekjoon_15727.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_15727 {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		if(N % 5 == 0) {
+			System.out.println(N / 5);
+		}else {
+			System.out.println((N / 5) + 1);
+		}
+	}
+
+}
