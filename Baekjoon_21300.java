595fbd3661df625a3351ef46e688c470457f83ab
goodunkuck <dkfldid45@naver.com>
2022-01-22 23:10:32 +0900

    Bottle Return

---
+++ Baekjoon_21300.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_21300 {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int sum = 0;
+		
+		for(int i = 0; i < 6; i++) {
+			int N = Integer.parseInt(st.nextToken());
+			sum += N;
+		}
+		
+		System.out.println(sum * 5);
+	}
+
+}
