e436144dbb90e2b40895b62f3b68e1d37f0f4196
goodunkuck <dkfldid45@naver.com>
2022-01-22 23:09:30 +0900

    세금

---
+++ Baekjoon_20492.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_20492 {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		System.out.println((int) (N * 0.78) + " " + (int) (N - ((N * 0.2) * 0.22)));
+	}
+
+}
