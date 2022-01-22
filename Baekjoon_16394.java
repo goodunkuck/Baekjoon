d495aa26651861119750c5555b095b96a686af57
goodunkuck <dkfldid45@naver.com>
2022-01-22 23:00:59 +0900

    홍익대학교

---
+++ Baekjoon_16394.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Baekjoon_16394 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		System.out.println(N - 1946);
+	}
+
+}
