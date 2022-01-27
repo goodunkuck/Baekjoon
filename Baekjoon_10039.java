07cd03e7998f07af5f21bb55b6875611c7a46a0a
goodunkuck <dkfldid45@naver.com>
2022-01-28 03:41:11 +0900

    평균 점수

--- Baekjoon_10039.java
+++ Baekjoon_10039.java
@@ -1,4 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Baekjoon_10039 {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int sum = 0;
+        for(int i = 0; i < 5; i++){
+            int N = Integer.parseInt(br.readLine());
+            //입력값이 40 미만이면 40으로 저장
+            if(N < 40){
+                N = 40;
+            }
+            sum += N;
+        }
+        System.out.println(sum / 5);
+	}
 
 }
