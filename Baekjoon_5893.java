3c80febc0008c1c6c17971b7a6c6303d9caa9c18
goodunkuck <dkfldid45@naver.com>
2022-01-28 13:50:10 +0900

    17배

--- Baekjoon_5893.java
+++ Baekjoon_5893.java
@@ -13,7 +13,7 @@ public class Baekjoon_5893 {
 		//최종으로 나온 10진수를 2진수로 저장할 stack
 		Stack<BigInteger> stack = new Stack<>();
 		
-		//N에 17을 곱하고 N에 다시 저장한다.
+		//N에 17을 곱하고 N에 다시 저장한다. multiply사용
 		N = N.multiply(BigInteger.valueOf(17));
 		
 		//2진수로 바꾸는 방법인 수에 2를 계속 나눠서 나머지와 최종 몫 1을 뽑아내는 방법이다.
