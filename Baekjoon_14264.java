f8d31735ee15cfa56883f7c23e8fbcae5141bde5
goodunkuck <dkfldid45@naver.com>
2022-01-28 13:46:19 +0900

    정육각형과 삼각형

--- Baekjoon_14264.java
+++ Baekjoon_14264.java
@@ -9,7 +9,7 @@ public class Baekjoon_14264 {
 		//소수 값이 나와야하므로 실수형 사용
 		double N = Integer.parseInt(br.readLine());
 		
-		//루트는 Maht.sqrt 제곱은 Math.pow 사용
+		//루트는 Maht.sqrt 제곱은 Math.pow 사용 
 		double area = (Math.sqrt(3) / 4) * Math.pow(N, 2);
 		
 		System.out.println(area);
