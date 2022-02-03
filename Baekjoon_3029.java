eb906d4e93273b10bbdb1e73528cd64dcf0cc676
goodunkuck <dkfldid45@naver.com>
2022-02-03 22:53:38 +0900

    경고

--- Baekjoon_3029.java
+++ Baekjoon_3029.java
@@ -11,7 +11,7 @@ public class Baekjoon_3029 {
 		String[] s = S.split(":");
 		String[] s2 = S2.split(":");
 		int[] N = new int[3];
-		
+		//int형으로 형변환 하면서 시간의 차를 시 분 초 기준으로 저장한다.
 		for(int i = 0; i < 3; i++) {
 			N[i] = Integer.parseInt(s2[i]) - Integer.parseInt(s[i]);
 		}
@@ -30,8 +30,8 @@ public class Baekjoon_3029 {
 			if(N[0] < 0) {
 				N[0] += 24;
 			}
+			//00:00:00방식으로 출력하기 위한 포맷
 			System.out.format("%02d:%02d:%02d", N[0], N[1], N[2]);
-			
 		}
 	}
 
