2809b509b222c1174c191a2be9b5cd4d81a6f011
goodunkuck <dkfldid45@naver.com>
2022-02-08 18:34:54 +0900

    텔레프라임

--- Baekjoon_7789.java
+++ Baekjoon_7789.java
@@ -10,8 +10,12 @@ public class Baekjoon_7789 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int A = Integer.parseInt(st.nextToken());
 		int B = Integer.parseInt(st.nextToken() + String.valueOf(A));
+		//나눠줄 변수
 		int i = 2;
+		//결과값을 저장할 변수
 		String S = "";
+		
+		//무한루프
 		while(true) {
 			if(i < A && A % i == 0) {
 				S = "No";
@@ -22,7 +26,9 @@ public class Baekjoon_7789 {
 				break;
 			}
 			
+			//나눠줄 변수에 반복될때마다 1씩 더한다.
 			i++;
+			//나너줄 변수가 B값과 같아지면 종료
 			if(i == B) {
 				S = "Yes";
 				break;
