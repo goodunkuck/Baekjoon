dcd3ec6c670f967735f7823253d7ad7b851e06f3
goodunkuck <dkfldid45@naver.com>
2022-03-15 23:28:45 +0900

    최대 힙

--- Baekjoon_11279.java
+++ Baekjoon_11279.java
@@ -9,11 +9,13 @@ public class Baekjoon_11279 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
+		//내림차순
 		PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
 		StringBuilder sb = new StringBuilder();
 		
 		for(int i = 0; i < N; i++) {
 			int x = Integer.parseInt(br.readLine());
+			//x가 0일때 비어있으면 0, 비어있지 않으면 맨 앞에 위치한 값을 제거, 출력
 			if(x == 0) {
 				if(pq.isEmpty()) sb.append(0).append("\n");
 				else {
